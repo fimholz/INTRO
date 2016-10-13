c6fb40d5e27d23abf050ff87ac63241a45e92e05
FENGERBOOK\Adrian <@>
2016-10-13 15:11:56 +0200

    one more try! :-)

--- INTRO_Common/Event.c
+++ INTRO_Common/Event.c
@@ -71,8 +71,9 @@ bool EVNT_EventIsSetAutoClear(EVNT_Handle event) {
 void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
    /* Handle the one with the highest priority. Zero is the event with the highest priority. */
    EVNT_Handle event;
-   /* \todo Make it reentrant */
+  CS1_CriticalVariable()
 
+  CS1_EnterCritical();
    for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++) { /* does a test on every event */
      if (GET_EVENT(event)) { /* event present? */
        if (clearEvent) {
@@ -81,6 +82,8 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
        break; /* get out of loop */
      }
    }
+   CS1_ExitCritical();
+
    if (event != EVNT_NOF_EVENTS) {
      callback(event);
      /* Note: if the callback sets the event, we will get out of the loop.
