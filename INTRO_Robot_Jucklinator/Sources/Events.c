1efd1908becc762ca630c668f9bb97773983c751
fimholz <frowin.imholz@gmail.com>
2016-10-14 16:13:31 +0200

    Enabled KeyInterrupts

--- INTRO_Robot_Jucklinator/Sources/Events.c
+++ INTRO_Robot_Jucklinator/Sources/Events.c
@@ -73,6 +73,23 @@ void TI1_OnInterrupt(void)
   /* Write your code here ... */
 }
 
+/*
+** ===================================================================
+**     Event       :  SW1_OnInterrupt (module Events)
+**
+**     Component   :  SW1 [ExtInt]
+**     Description :
+**         This event is called when an active signal edge/level has
+**         occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void SW1_OnInterrupt(void)
+{
+  /* Write your code here ... */
+}
+
 /* END Events */
 
 #ifdef __cplusplus
