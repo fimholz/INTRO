095ac326284154807b524f12ee46228b269ce94a
fimholz <frowin.imholz@gmail.com>
2016-11-17 13:15:05 +0100

    prettier code on some positions

--- INTRO_Common/Sem.c
+++ INTRO_Common/Sem.c
@@ -20,9 +20,6 @@
 #include "Sem.h"
 #include "LED.h"
 
-#define USE_SEMAPHORES 1
-
-#if USE_SEMAPHORES
 static void vSlaveTask(void *pvParameters) {
   xSemaphoreHandle sem = (xSemaphoreHandle)pvParameters;
 
@@ -58,17 +55,14 @@ static void vMasterTask(void *pvParameters) {
     vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
-#endif /* USE_SEMAPHORES */
 
 void SEM_Deinit(void) {
 }
 
 /*! \brief Initializes module */
 void SEM_Init(void) {
-#if USE_SEMAPHORES
   if (xTaskCreate(vMasterTask, "Master", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
     for(;;){} /* error */
   }
-#endif
 }
 #endif /* PL_CONFIG_HAS_SEMAPHORE */
